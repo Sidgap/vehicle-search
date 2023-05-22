49446a98b52713e45fafa7a9ec89602d8785ab8c
Sidgap <sidhantsharma712@gmail.com>
2023-05-22 13:44:50 +0530

    Initial setup

---
+++ src/main/java/com/learn2code/vehicleapi/serachn/VehicleSearchnApplication.java
@@ -0,0 +1,13 @@
+package com.learn2code.vehicleapi.serachn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleSearchnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleSearchnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn2code/vehicleapi/serachn/VehicleSearchnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn2code.vehicleapi.serachn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleSearchnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
